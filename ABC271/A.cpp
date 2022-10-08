f29daa0544cf107adc59e93bc87465f249c08259
hpumengzhao <2312018660@qq.com>
2022-10-08 17:39:16 +0800

    add A

---
+++ ABC271/A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+const ll mod = 1e9+7;
+#define pb push_back
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define fep(i,a,b) for(int i=(a);i>=(b);i--)
+#define deb(x) cerr<<#x<<" = "<<(x)<<"\n"
+typedef vector<int> VI;
+typedef vector<ll> VII;
+typedef pair<int,int> pii;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+string C = "0123456789ABCDEF";
+
+int main(){
+	int N;
+	cin >> N;
+	int p = N % 16;
+	int D = N / 16;
+	cout << C[D] << C[p] << endl;
+}
